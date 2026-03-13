236e99f4e88c159ac64144a250ab8648a9473253
SamkovLuka <staddycase@gmail.com>
2026-03-13 16:14:19 +0200

    Added ticket pagination and statistics charts

--- appeals/views.py
+++ appeals/views.py
@@ -1,17 +1,40 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
+from django.db.models import Q, Count
 from django.http import HttpResponse
-from appeals.models import Appeal
-from appeals.forms import AppealForm
-from appeals.forms import CommentForm
-from .models import Appeal, Comment
+from appeals.models import Appeal, Comment
+from appeals.forms import AppealForm, CommentForm
 from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator
 
 @login_required
 def admin_panel(request):
+    q = request.GET.get("q", "").strip()
     appeals = Appeal.objects.all().order_by("-created_at")
-    return render(request, "appeals/adminpanel.html", {"appeals": appeals})
+
+    if q:
+        appeals = appeals.filter(
+            Q(title__icontains=q) |
+            Q(description__icontains=q) |
+            Q(category__icontains=q) |
+            Q(status__icontains=q) |
+            Q(author__username__icontains=q)
+        ).distinct()
+
+    paginator = Paginator(appeals, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    status_stats = Appeal.objects.values('status').annotate(count=Count('id'))
+    user_stats = Appeal.objects.values('author__username').annotate(count=Count('id'))
+
+    return render(request, "appeals/adminpanel.html", {
+        "appeals": page_obj,
+        "q": q,
+        "page_obj": page_obj,
+        "status_stats": status_stats,
+        "user_stats": user_stats
+    })
 
 @login_required
 def appeal_create(request):
@@ -24,7 +47,6 @@ def appeal_create(request):
             return redirect("/appeals/adminpanel")
     else:
         appeal_form = AppealForm()
-    
     return render(request, "appeals/create.html", {"form": appeal_form})
 
 @login_required
@@ -37,7 +59,6 @@ def appeal_update(request, pk):
             return redirect("/appeals/adminpanel")
     else:
         appeal_form = AppealForm(instance=appeal)
-    
     return render(request, "appeals/update.html", {"form": appeal_form})
 
 @login_required
@@ -60,21 +81,16 @@ def appeal_status(request, pk):
 @login_required
 def appeal_detail(request, pk):
     appeal = get_object_or_404(Appeal, pk=pk)
-    
-   
     edit_comment_id = request.GET.get('edit')
 
     if request.method == "POST":
         action = request.POST.get("action")
         comment_id = request.POST.get("comment_id")
 
-      
         if action == "delete" and comment_id:
             comment = get_object_or_404(Comment, id=comment_id, author=request.user)
             comment.delete()
             return redirect("appeals:appeal_detail", pk=appeal.pk)
-
-       
         elif action == "edit" and comment_id:
             comment = get_object_or_404(Comment, id=comment_id, author=request.user)
             new_text = request.POST.get("text")
@@ -82,8 +98,6 @@ def appeal_detail(request, pk):
                 comment.text = new_text
                 comment.save()
             return redirect("appeals:appeal_detail", pk=appeal.pk)
-
-       
         else:
             form = CommentForm(request.POST)
             if form.is_valid():
@@ -101,30 +115,11 @@ def appeal_detail(request, pk):
         "edit_comment_id": int(edit_comment_id) if edit_comment_id else None,
     })
 
-@login_required
-def admin_panel(request):
-    q = request.GET.get("q", "").strip()
-    appeals = Appeal.objects.all().order_by("-created_at")
-
-    if q:
-        appeals = appeals.filter(
-            Q(title__icontains=q) |
-            Q(description__icontains=q) |
-            Q(category__icontains=q) |
-            Q(status__icontains=q) |
-            Q(author__username__icontains=q)
-        ).distinct()
-
-    return render(request, "appeals/adminpanel.html", {"appeals": appeals, "q": q})
-
 @require_POST
 def update_status(request, pk):
     appeal = get_object_or_404(Appeal, pk=pk)
     new_status = request.POST.get('status')
-    
-   
     if new_status in Appeal.Status.values:
         appeal.status = new_status
         appeal.save()
-        
     return redirect(request.META.get('HTTP_REFERER', 'adminpanel'))
